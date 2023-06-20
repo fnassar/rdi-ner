fc717cfc67e5e4f29a01e5d6553b76c01f28a99c
Angil Azmy <angilazmy77@gmail.com>
2023-06-20 10:34:35 +0000

    added dataset

--- task1/dataset.py
+++ task1/dataset.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import Dataset as BaseDataset
 from torch.nn.utils.Inn import pad_sequence
-device= torch.device("cuda" if torch.cuda.is_avaliable() else"cpu")
+devic= torch.device("cuda" if torch.cuda.is_avaliable() else"cpu")
 tokenizer = AutoTokenizer.from_pretrained("CAMeL-Lab/bert-base-arabic-camelbert-mix-ner")
 def process(examples):
   tokenized = tokenizer(text,truncation=True, add_special_tokens=False,return_tensors="pt")
