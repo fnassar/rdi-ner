1c92d2ad9c22e2d7ddcfffe591da22eea0c81cb2
Angil Azmy <angilazmy77@gmail.com>
2023-06-20 14:02:10 +0000

    added model

--- task3/model.py
+++ task3/model.py
@@ -1,13 +1,13 @@
-from transformers import BertForTokenClassification, BertTokenizer
+from transformers import BertForTokenClassification
 class Model(BertForTokenClassification):
     def __init__(self, flag):
       if flag:
           self.model = BertForTokenClassification.from_pretrained("CAMeL-Lab/bert-base-arabic-camelbert-mix-ner")
       else:
           self.model = BertForTokenClassification.from_json_file()
-      self.base_model = self.model.layers[:11]
-      self.dense1 = nn.Linear(300, 10)
+      # self.base_model = self.model.layers[:11]
+      # self.dense1 = nn.Linear(300, 10)
     def forward(self, inputs):
         outputs = self.model(inputs)
-        logits = self.dense1(outputs)
+        # logits = self.dense1(outputs)
         return outputs
