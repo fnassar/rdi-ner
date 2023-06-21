71e60a2644d865004ee1ccdad02224a54d4aa034
Angil Azmy <angilazmy77@gmail.com>
2023-06-21 05:08:47 +0000

    added model

--- task3/model.py
+++ task3/model.py
@@ -1,10 +1,14 @@
-from transformers import BertForTokenClassification
-class Model(BertForTokenClassification):
+from transformers import BertForTokenClassification, AutoTokenizer
+from torch.nn.modules import module
+
+class Model(nn.module):
     def __init__(self, flag):
       if flag:
           self.model = BertForTokenClassification.from_pretrained("CAMeL-Lab/bert-base-arabic-camelbert-mix-ner")
       else:
           self.model = BertForTokenClassification.from_json_file()
+      def get_model(self):
+        return self.model    
       # self.base_model = self.model.layers[:11]
       # self.dense1 = nn.Linear(300, 10)
     def forward(self, inputs):
